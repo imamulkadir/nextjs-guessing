1a6671d11b5eb40bbeaaf7dae3a38aa4ab9ea4dd
Imamul Kadir <imamulkadiroyan@gmail.com>
2023-11-28 05:08:05 +0600

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
   };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      {/* <div className="p-4 shadow-md bg-white rounded-md">
-        <h1 className="text-2xl font-semibold mb-4 text-black">
-          Enter your name
-        </h1>
-      </div> */}
       <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <div className="p-0 mb-10 text-center bg-transparent rounded-md">
+          <h1 className="text-2xl font-semibold mb-0 text-gray-500">
+            Enter First or Last name to predict!!!
+          </h1>
+        </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
             type="text"
-            placeholder="Type any human name..."
-            className="block text-center py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder="Hasina"
+            className="block text-center py-2.5 px-0 w-full text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             value={inputVal}
             required
             onChange={(e) => {
@@ -35,7 +35,7 @@ export default function Home() {
           type="submit"
           className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
         >
-          Guess
+          Predict
         </button>
       </form>
     </div>
